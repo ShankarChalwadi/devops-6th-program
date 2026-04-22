9924090f32e205c0fb0fdf7e3591d3fa6278f56e
Shankar Chalwadi <shankarchalwadi142@gmail.com>
2026-04-22 12:56:03 +0530

    initial commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+
+public class GoogleTest {
+    @Test
+    public void openWebsite() throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.entrepreneur.com/");
+        System.out.println("Title:"+driver.getTitle());
+        Thread.sleep(15000);
+        driver.quit();
+    }
+}
